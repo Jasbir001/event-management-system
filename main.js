d8bda857440d3b4832e7c95e6083c3031a177dbf
Anchu749 <jasbirverma441441@gmail.com>
2026-05-26 09:47:22 +0530

    Fix Vercel deployment config

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+module.exports = app;
