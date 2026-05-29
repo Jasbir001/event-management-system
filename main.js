112e2a655c317625927ebcc6fc6c2135fc408bff
Anchu749 <jasbirverma441441@gmail.com>
2026-05-29 20:23:47 +0530

    fix: remove frontend static file serving

--- main.js
+++ main.js
@@ -12,23 +12,15 @@ app.use(cors({
   origin: true,
   credentials: true
 }));
-app.use(express.json()); // for JSON APIs
+app.use(express.json());
 
-// Backend API Routes
 app.use('/api', require('./backend/routes/api'));
-
-// Serve Frontend in Production
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
-});
 
 if (require.main === module) {
   const initTables = require('./backend/database/init_tables');
   
   app.listen(port, async () => {
     console.log(`Server is running on port ${port}`);
-    // Automatically create database tables if they do not exist
     await initTables();
   });
 }
